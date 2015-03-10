dc81d772fbcdf3349cbc0d4d80c9eeb207ae5ac7
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2015-03-10 18:34:18 +0100

    last commit

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 /**
  * Created by arnaud on 25/02/15.
  */
-var app = angular.module('contactApp', ['ngRoute']);
+var app = angular.module('contactApp', ['ngRoute', 'firebase']);
 
-app.controller('homeController', function($scope, ContactRepository) {
+app.controller('homeController', function ($scope, ContactRepository) {
     $scope.contacts = ContactRepository.list();
 });
 
-app.controller('ContactAddController', function($scope, ContactRepository) {
+app.controller('ContactAddController', function ($scope, ContactRepository) {
     $scope.formData = {};
     $scope.displayForm = true;
-    $scope.create = function() {
+    $scope.create = function () {
         ContactRepository.create($scope.formData);
         $scope.displayForm = false;
     }
 });
 
-app.controller('ContactEditController', function($scope, ContactRepository, $routeParams) {
+app.controller('ContactEditController', function ($scope, ContactRepository, $routeParams) {
     $scope.contact = ContactRepository.find($routeParams.id);
     $scope.displayForm = true;
-    $scope.update = function() {
-        ContactRepository.update($routeParams.id, $scope.contact);
+    $scope.update = function () {
+        ContactRepository.update($scope.contact);
         $scope.displayForm = false;
     }
 });
 
-app.controller('ContactDeleteController', function($scope, ContactRepository, $routeParams) {
+app.controller('ContactDeleteController', function ($scope, ContactRepository, $routeParams) {
     $scope.contact = ContactRepository.find($routeParams.id);
     $scope.displayForm = true;
-    $scope.delete = function() {
-        ContactRepository.delete($routeParams.id);
+    $scope.delete = function () {
+        ContactRepository.delete($scope.contact);
         $scope.displayForm = false;
     }
 });
@@ -40,84 +40,64 @@ app.config(function ($routeProvider) {
         templateUrl: 'views/home.html',
         controller: 'homeController'
     })
-    .when('/add', {
-        templateUrl: 'views/add.html',
-        controller: 'ContactAddController'
-    })
-    .when('/edit/:id', {
-        templateUrl: 'views/edit.html',
-        controller: 'ContactEditController'
-    })
-    .when('/delete/:id', {
-        templateUrl: 'views/delete.html',
-        controller: 'ContactDeleteController'
-    })
-    .otherwise({
-        redirectTo: '/home'
-    });
+        .when('/add', {
+            templateUrl: 'views/add.html',
+            controller: 'ContactAddController'
+        })
+        .when('/edit/:id', {
+            templateUrl: 'views/edit.html',
+            controller: 'ContactEditController'
+        })
+        .when('/delete/:id', {
+            templateUrl: 'views/delete.html',
+            controller: 'ContactDeleteController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
 });
 
-app.directive('email', function() {
+app.directive('email', function () {
     return {
         restrict: 'E',
         template: '<a href="mailto:{{ contact.email }}" ><img src="http://goo.gl/AlXUGZ" alt="contact"/>  {{ contact.email }}</a> '
     };
 });
 
-app.provider('ContactRepository', function() {
-    var contacts = [
-        {
-            "picture": "http://lorempixel.com/90/90/",
-            "age": 65,
-            "firstname": "ethel",
-            "name": "logan",
-            "email": "ethellogan@tsunamia.com",
-            "phone": "+1 (857) 470-3091"
-        },
-        {
-            "picture": "http://lorempixel.com/90/90/",
-            "age": 38,
-            "firstname": "carol",
-            "name": "vasquez",
-            "email": "carolvasquez@lexicondo.com",
-            "phone": "+1 (853) 507-3026"
-        },
-        {
-            "picture": "http://lorempixel.com/90/90/",
-            "age": 43,
-            "firstname": "chan",
-            "name": "bowman",
-            "email": "chanbowman@vicon.com",
-            "phone": "+1 (994) 412-2204"
-        }
-    ];
+app.constant('FirebaseUrl', 'https://radiant-heat-4732.firebaseio.com/');
 
-    this.$get = function() {
-        return {
-            list: function () {
+app.service('ContactRepository', function (FirebaseUrl, $firebase) {
 
-                return contacts;
-            },
+    var ref = new Firebase(FirebaseUrl + '/users');
 
-            find: function (id) {
+    this.list = function () {
+        var sync = $firebase(ref);
 
-                return contacts[id];
-            },
+        return sync.$asArray();
+    };
 
-            create: function (data) {
+    this.find = function (id) {
+        var refFind = ref.child(id);
 
-                contacts.push(data);
+        var sync = $firebase(refFind);
 
-                return contacts.length - 1;
-            },
+        return sync.$asObject();
+    };
 
-            update: function (id, data) {
-                contacts[id] = data;
-            },
+    this.create = function (data) {
+        var sync = $firebase(ref);
 
-            delete: function (id) {
-                contacts.splice(id, 1);
-            }
-        };
-    }
+        sync.$push(data).then(function (newChildRef) {
+            console.log("added record with id " + newChildRef.key());
+        });
+    };
+
+    this.update = function (element) {
+        element.$save();
+    };
+
+    this.delete = function (element) {
+
+        element.$remove();
+    };
 });
